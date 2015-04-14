b26f787910ff1b4dc37e6e91b0477adb9b7dea88
ClintFowler <jedd1876@gmail.com>
2015-04-13 19:46:24 -0600

    Putting all original files up to account.

---
+++ public/scripts/script.js
@@ -0,0 +1,36 @@
+function myFunction() {
+
+	var username = $("#username").val();
+	var password = $("#password").val();
+	var dataString = 'username=' + username + '&password=' + password;
+
+	if (username == '' || password == '')
+	{
+		alert("Please Fill All Fields");
+	}
+	else
+	{
+		$.ajax({
+			type: "POST",
+			url: "http://localhost:8080/api/auth",
+			data: dataString,
+			cache: false,
+			dataType:'JSON',
+			statusCode:
+			{
+				// ok
+				200: function(){
+					//alert('access granted');
+					document.getElementById("message").innerHTML = "access granted";
+					$("#registerlink").hide();
+				},
+				// unauthorized
+				401: function(){
+					//alert('access denied')
+					document.getElementById("message").innerHTML = "access denied";
+					$("#registerlink").show();
+				}
+			}
+		});
+	}
+}
